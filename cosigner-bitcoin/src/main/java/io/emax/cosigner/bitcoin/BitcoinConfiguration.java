158e372189eb0f60812a8d94570e341f66dd5069
Tom Robichaud <tom.robichaud@gmail.com>
2015-11-12 13:33:38 -0400

    Security provider changes, unit tests are failing in some places.

--- cosigner-bitcoin/src/main/java/io/emax/cosigner/bitcoin/BitcoinConfiguration.java
+++ cosigner-bitcoin/src/main/java/io/emax/cosigner/bitcoin/BitcoinConfiguration.java
@@ -3,6 +3,7 @@ package io.emax.cosigner.bitcoin;
 import io.emax.cosigner.api.currency.SigningType;
 import io.emax.cosigner.common.EnvironmentVariableParser;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,6 +11,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.security.Security;
 import java.util.Properties;
 
 public class BitcoinConfiguration implements io.emax.cosigner.api.currency.CurrencyConfiguration {
@@ -32,6 +34,7 @@ public class BitcoinConfiguration implements io.emax.cosigner.api.currency.Curre
 
   public BitcoinConfiguration() {
     loadConfig();
+    Security.addProvider(new BouncyCastleProvider());
   }
 
   private static synchronized void loadConfig() {
