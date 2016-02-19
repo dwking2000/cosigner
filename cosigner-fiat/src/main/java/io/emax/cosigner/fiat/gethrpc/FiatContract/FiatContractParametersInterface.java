ac2247321a6fa3e2220191798f20e292286e38f1
Tom Robichaud <tom.robichaud@gmail.com>
2016-02-19 12:12:52 -0400

    Finishing up parameter bytecode methods.

--- cosigner-fiat/src/main/java/io/emax/cosigner/fiat/gethrpc/FiatContract/FiatContractParametersInterface.java
+++ cosigner-fiat/src/main/java/io/emax/cosigner/fiat/gethrpc/FiatContract/FiatContractParametersInterface.java
@@ -1,4 +1,26 @@
 package io.emax.cosigner.fiat.gethrpc.FiatContract;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FiatContractParametersInterface {
+  String createContract(String adminAddress, List<String> ownerAddresses,
+      int numSignaturesRequired);
+
+  String createTokens(long numTokens);
+
+  String destroyTokens(long numTokens);
+
+  String getOwners();
+
+  String getBalance(String address);
+
+  String getTotalBalance();
+
+  String isOwner(String address);
+
+  String transfer(long nonce, String sender, List<String> recipients, List<Long> amount,
+      List<String> sigV, List<String> sigR, List<String> sigS);
+
+  Map<String, List<String>> parseTransfer(String bytecode);
 }
--- cosigner-fiat/src/main/java/io/emax/cosigner/fiat/gethrpc/FiatContract/v1/FiatContractParametersV1.java
+++ cosigner-fiat/src/main/java/io/emax/cosigner/fiat/gethrpc/FiatContract/v1/FiatContractParametersV1.java
@@ -4,12 +4,15 @@ import io.emax.cosigner.common.ByteUtilities;
 import io.emax.cosigner.fiat.gethrpc.FiatContract.FiatContractParametersInterface;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class FiatContractParametersV1 implements FiatContractParametersInterface {
-  // TODO For each function, create a method that takes arguments and converts them to bytecode
-  // TODO Create a method that takes bytecode for a transfer and converts it back to arguments
 
+  @Override
   public String createContract(String adminAddress, List<String> ownerAddresses,
       int numSignaturesRequired) {
     // Format should be initData/Payload/Arguments
@@ -47,6 +50,7 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
+  @Override
   public String createTokens(long numTokens) {
     FiatContractV1 contract = new FiatContractV1();
     String response = contract.getCreateTokens();
@@ -58,6 +62,7 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
+  @Override
   public String destroyTokens(long numTokens) {
     FiatContractV1 contract = new FiatContractV1();
     String response = contract.getDestroyTokens();
@@ -69,6 +74,7 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
+  @Override
   public String getOwners() {
     FiatContractV1 contract = new FiatContractV1();
     String response = contract.getGetOwners();
@@ -76,6 +82,7 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
+  @Override
   public String getBalance(String address) {
     FiatContractV1 contract = new FiatContractV1();
     String response = contract.getGetBalance();
@@ -86,6 +93,7 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
+  @Override
   public String getTotalBalance() {
     FiatContractV1 contract = new FiatContractV1();
     String response = contract.getGetTotalBalance();
@@ -93,6 +101,7 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
+  @Override
   public String isOwner(String address) {
     FiatContractV1 contract = new FiatContractV1();
     String response = contract.getIsOwner();
@@ -103,6 +112,196 @@ public class FiatContractParametersV1 implements FiatContractParametersInterface
     return response;
   }
 
-  // TODO Transfer
-  // TODO Parse Transfer Bytecode
+  @Override
+  public String transfer(long nonce, String sender, List<String> recipients, List<Long> amount,
+      List<String> sigV, List<String> sigR, List<String> sigS) {
+    // We expect the list of sig's to be in order such that the index of each matches up.
+    // We expect that the recipients and amounts be in order such that the index of each matches up.
+    // I.E. sigV[2] belongs with sigR[2] and sigS[2].
+
+    FiatContractV1 contract = new FiatContractV1();
+    String response = contract.getTransfer();
+
+    long numberOfParams = 7;
+    long sizeOfPreviousArrays = 0;
+
+    // Nonce
+    String formattedString = ByteUtilities.toHexString(BigInteger.valueOf(nonce).toByteArray());
+    formattedString = String.format("%64s", formattedString).replace(' ', '0');
+    response += formattedString;
+
+    // Sender
+    formattedString = String.format("%64s", sender).replace(' ', '0');
+    response += formattedString;
+
+    // Recipient Pointer
+    formattedString = String.format("%64s", calculatePointer(numberOfParams, sizeOfPreviousArrays))
+        .replace(' ', '0');
+    response += formattedString;
+    sizeOfPreviousArrays += recipients.size() + 1;
+
+    // Amount Pointer
+    formattedString = String.format("%64s", calculatePointer(numberOfParams, sizeOfPreviousArrays))
+        .replace(' ', '0');
+    response += formattedString;
+    sizeOfPreviousArrays += amount.size() + 1;
+
+    // SigV Pointer
+    formattedString = String.format("%64s", calculatePointer(numberOfParams, sizeOfPreviousArrays))
+        .replace(' ', '0');
+    response += formattedString;
+    sizeOfPreviousArrays += sigV.size() + 1;
+
+    // SigR Pointer
+    formattedString = String.format("%64s", calculatePointer(numberOfParams, sizeOfPreviousArrays))
+        .replace(' ', '0');
+    response += formattedString;
+    sizeOfPreviousArrays += sigR.size() + 1;
+
+    // SigS Pointer
+    formattedString = String.format("%64s", calculatePointer(numberOfParams, sizeOfPreviousArrays))
+        .replace(' ', '0');
+    response += formattedString;
+    sizeOfPreviousArrays += sigS.size() + 1;
+
+    // Recipient Data
+    response += serializeStringList(recipients);
+
+    // Amount Data
+    response += serializeLongList(amount);
+
+    // SigV Data
+    response += serializeStringList(sigV);
+
+    // SigR Data
+    response += serializeStringList(sigR);
+
+    // SigS Data
+    response += serializeStringList(sigS);
+
+    return response;
+  }
+
+  private String calculatePointer(long numberOfParams, long sizeOfPreviousArrays) {
+    return ByteUtilities.toHexString(
+        BigInteger.valueOf(32 * (numberOfParams + sizeOfPreviousArrays)).toByteArray());
+  }
+
+  private String serializeStringList(List<String> data) {
+    String response = "";
+
+    String formattedString =
+        ByteUtilities.toHexString(BigInteger.valueOf(data.size()).toByteArray());
+    formattedString = String.format("%64s", formattedString).replace(' ', '0');
+    response += formattedString;
+
+    for (String dataItem : data) {
+      formattedString = String.format("%64s", dataItem).replace(' ', '0');
+      response += formattedString;
+    }
+
+    return response;
+  }
+
+  private String serializeLongList(List<Long> data) {
+    String response = "";
+
+    String formattedString =
+        ByteUtilities.toHexString(BigInteger.valueOf(data.size()).toByteArray());
+    formattedString = String.format("%64s", formattedString).replace(' ', '0');
+    response += formattedString;
+
+    for (Long dataItem : data) {
+      formattedString = ByteUtilities.toHexString(BigInteger.valueOf(dataItem).toByteArray());
+      formattedString = String.format("%64s", formattedString).replace(' ', '0');
+      response += formattedString;
+    }
+
+    return response;
+  }
+
+  @Override
+  public Map<String, List<String>> parseTransfer(String bytecode) {
+    FiatContractV1 contract = new FiatContractV1();
+    int bufPointer = 0;
+    final int paramSize = 64;
+    String readValue = "";
+    BigInteger intValue = BigInteger.ZERO;
+    HashMap<String, List<String>> parameters = new HashMap<>();
+
+    // Check that this is a transfer command.
+    String function = bytecode.substring(bufPointer, contract.getTransfer().length());
+    bufPointer += contract.getTransfer().length();
+    if (!function.equalsIgnoreCase(contract.getTransfer())) {
+      return null;
+    }
+
+    // Nonce
+    readValue = bytecode.substring(bufPointer, paramSize);
+    bufPointer += paramSize;
+    intValue = new BigInteger("00" + readValue, 16);
+    parameters.put("nonce", Arrays.asList(intValue.toString(10)));
+
+    // Sender
+    readValue = bytecode.substring(bufPointer, paramSize);
+    bufPointer += paramSize;
+    readValue = ByteUtilities
+        .toHexString(ByteUtilities.stripLeadingNullBytes(ByteUtilities.toByteArray(readValue)));
+    parameters.put("sender", Arrays.asList(readValue));
+
+    // Recipient
+    parameters.put("recipients", parseArray(bufPointer, bytecode, false));
+    bufPointer += paramSize;
+
+    // Amount
+    parameters.put("amount", parseArray(bufPointer, bytecode, true));
+    bufPointer += paramSize;
+
+    // SigV
+    parameters.put("sigV", parseArray(bufPointer, bytecode, false));
+    bufPointer += paramSize;
+
+    // SigR
+    parameters.put("sigR", parseArray(bufPointer, bytecode, false));
+    bufPointer += paramSize;
+
+    // SigS
+    parameters.put("sigS", parseArray(bufPointer, bytecode, false));
+    bufPointer += paramSize;
+
+    return parameters;
+  }
+
+  private List<String> parseArray(int bufPointer, String buffer, boolean decodeToInt) {
+    final int paramSize = 64;
+    // Find the location of the array data
+    String readValue = buffer.substring(bufPointer, paramSize);
+    BigInteger intValue = new BigInteger("00" + readValue, 16);
+    int arrayPointer = intValue.intValue() * 2; // String vs Bytes
+
+    // Get the size of the array
+    readValue = buffer.substring(arrayPointer, paramSize);
+    arrayPointer += paramSize;
+    intValue = new BigInteger("00" + readValue, 16);
+    int arraySize = intValue.intValue();
+
+    // Read in the data
+    LinkedList<String> arrayData = new LinkedList<>();
+    for (int i = 0; i < arraySize; i++) {
+      readValue = buffer.substring(arrayPointer, paramSize);
+      arrayPointer += paramSize;
+      readValue = ByteUtilities
+          .toHexString(ByteUtilities.stripLeadingNullBytes(ByteUtilities.toByteArray(readValue)));
+
+      // If we want the int value of the hex data
+      if (decodeToInt) {
+        intValue = new BigInteger("00" + readValue, 16);
+        readValue = intValue.toString(10);
+      }
+
+      arrayData.add(readValue);
+    }
+
+    return arrayData;
+  }
 }
