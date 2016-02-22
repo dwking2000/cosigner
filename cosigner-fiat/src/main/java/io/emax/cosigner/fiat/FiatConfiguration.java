fa97e23d096f98c610da3b9f1819f231a890c328
Tom Robichaud <tom.robichaud@gmail.com>
2016-02-22 11:26:55 -0400

    FiatWallet work

--- cosigner-fiat/src/main/java/io/emax/cosigner/fiat/FiatConfiguration.java
+++ cosigner-fiat/src/main/java/io/emax/cosigner/fiat/FiatConfiguration.java
@@ -3,23 +3,89 @@ package io.emax.cosigner.fiat;
 import io.emax.cosigner.api.currency.CurrencyConfiguration;
 import io.emax.cosigner.api.currency.SigningType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Properties;
+
 public class FiatConfiguration implements CurrencyConfiguration {
-  private static String serverKey = "deadbeef";
+  private static final Logger LOGGER = LoggerFactory.getLogger(FiatConfiguration.class);
+
+  private static String currencySymbol = "FIAT";
+  private static String serverPrivateKey = "deadbeef";
+  private static int minSignatures = 10;
+  private static int minConfirmations = 10;
+  private static long gasPrice = 100000000000L;
+  private static long contractGas = 3000000L;
+  private static String contractAccount = "4839540a0ae3242fadf288622f7de1a9278a5858";
+  private static String adminAccount = "4839540a0ae3242fadf288622f7de1a9278a5858";
+  private static String[] multiSigAccounts = {"4839540a0ae3242fadf288622f7de1a9278a5858"};
+  private static boolean generateNewContract = true;
+  private static String contractAddress = "";
+  private static BigDecimal maxAmountPerHour = BigDecimal.ZERO;
+  private static BigDecimal maxAmountPerDay = BigDecimal.ZERO;
+  private static BigDecimal maxAmountPerTransaction = BigDecimal.ZERO;
+
+  private static boolean configLoaded = false;
+
+  public FiatConfiguration(String currency) {
+    loadConfig(currency);
+  }
+
+  /*private static long getLongProp(Properties prop, String value, long defaultValue) {
+    try {
+      return Long.parseLong(prop.getProperty(value));
+    } catch (Exception e) {
+      LOGGER.warn(null, e);
+      return defaultValue;
+    }
+  }*/
+
+  private synchronized void loadConfig(String currency) {
+    if (!configLoaded) {
+      configLoaded = true;
+
+      FileInputStream propertiesFile = null;
+      String propertiesFilePath = "./cosigner-" + currency + ".properties";
+
+      try {
+        Properties cosignerProperties = new Properties();
+        propertiesFile = new FileInputStream(propertiesFilePath);
+
+        cosignerProperties.load(propertiesFile);
+        propertiesFile.close();
+
+        // TODO Load properties file.
+      } catch (IOException e) {
+        if (propertiesFile != null) {
+          try {
+            propertiesFile.close();
+          } catch (IOException e1) {
+            LOGGER.warn(null, e1);
+          }
+        }
+        LOGGER.debug(null, e);
+        LOGGER.info("Could not load cosigner-fiat configuration, using defaults.");
+      }
+    }
+  }
 
   @Override
   public String getCurrencySymbol() {
-    // TODO Needs to be configurable.
-    return null;
+    return currencySymbol;
   }
 
   @Override
   public SigningType getSigningType() {
-    return null;
+    return SigningType.SENDALL;
   }
 
   @Override
   public int getMinSignatures() {
-    return 0;
+    return minSignatures;
   }
 
   @Override
@@ -29,10 +95,54 @@ public class FiatConfiguration implements CurrencyConfiguration {
 
   @Override
   public boolean hasMultipleRecipients() {
-    return false;
+    return true;
   }
 
-  public String getServerKey() {
-    return serverKey;
+  public String getServerPrivateKey() {
+    return serverPrivateKey;
+  }
+
+  public int getMinConfirmations() {
+    return minConfirmations;
+  }
+
+  public long getGasPrice() {
+    return gasPrice;
+  }
+
+  public long getContractGas() {
+    return contractGas;
+  }
+
+  public String getContractAccount() {
+    return contractAccount;
+  }
+
+  public String getAdminAccount() {
+    return adminAccount;
+  }
+
+  public String[] getMultiSigAccounts() {
+    return multiSigAccounts;
+  }
+
+  public BigDecimal getMaxAmountPerHour() {
+    return maxAmountPerHour;
+  }
+
+  public BigDecimal getMaxAmountPerDay() {
+    return maxAmountPerDay;
+  }
+
+  public BigDecimal getMaxAmountPerTransaction() {
+    return maxAmountPerTransaction;
+  }
+
+  public String getContractAddress() {
+    return contractAddress;
+  }
+
+  public boolean generateNewContract() {
+    return generateNewContract;
   }
 }
--- cosigner-fiat/src/main/java/io/emax/cosigner/fiat/FiatWallet.java
+++ cosigner-fiat/src/main/java/io/emax/cosigner/fiat/FiatWallet.java
@@ -6,12 +6,25 @@ import io.emax.cosigner.common.ByteUtilities;
 import io.emax.cosigner.common.crypto.Secp256k1;
 import io.emax.cosigner.ethereum.EthereumResource;
 import io.emax.cosigner.ethereum.common.EthereumTools;
+import io.emax.cosigner.ethereum.common.RlpItem;
+import io.emax.cosigner.ethereum.common.RlpList;
+import io.emax.cosigner.ethereum.gethrpc.CallData;
+import io.emax.cosigner.ethereum.gethrpc.DefaultBlock;
 import io.emax.cosigner.ethereum.gethrpc.EthereumRpc;
+import io.emax.cosigner.ethereum.gethrpc.RawTransaction;
+import io.emax.cosigner.fiat.gethrpc.FiatContract.FiatContract;
+import io.emax.cosigner.fiat.gethrpc.FiatContract.FiatContractInterface;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 
 public class FiatWallet implements Wallet {
@@ -19,17 +32,91 @@ public class FiatWallet implements Wallet {
 
   // RPC and configuration
   private static final EthereumRpc ethereumRpc = EthereumResource.getResource().getGethRpc();
-  private static final FiatConfiguration config = new FiatConfiguration();
+  private static FiatConfiguration config;
 
+  private String contractAddress = "";
+  private FiatContractInterface contractInterface = new FiatContract();
   private HashSet<String> knownAddresses = new HashSet<>();
+  private HashMap<String, HashSet<String>> ownedAddresses = new HashMap<>();
 
-  // TODO Need a method that scans for contracts on the main account and finds any existing fiat contract automatically.
-  //      This should take a currency symbol and pick up the one we're after.
-  //      I.E. Cosigner registers a FiatWallet("EUR"), which does a scan(),
-  //           and either creates a new contract, or uses the existing contract if it exists.
-  // TODO Consider an auto-upgrade process on a new contract version. Probably not needed though.
-  // TODO Make it possible to specify the contract address, we should know what it is.
-  // TODO Make an option to create the contract if one doesn't exist, for first run, etc...
+  public FiatWallet(String currency) {
+    config = new FiatConfiguration(currency);
+    setupFiatContract();
+  }
+
+  private void setupFiatContract() {
+    if (config.getContractAddress() != null && !config.getContractAddress().isEmpty()) {
+      contractAddress = config.getContractAddress();
+      contractInterface = getContractType(contractAddress);
+    } else {
+      try {
+        String txCount = ethereumRpc.eth_getTransactionCount("0x" + config.getContractAccount(),
+            DefaultBlock.LATEST.toString());
+        int rounds = new BigInteger(1, ByteUtilities.toByteArray(txCount)).intValue();
+        for (int i = 0; i < rounds; i++) {
+          RlpList contractAddress = new RlpList();
+          RlpItem contractCreator =
+              new RlpItem(ByteUtilities.toByteArray(config.getContractAccount()));
+          RlpItem nonce =
+              new RlpItem(ByteUtilities.stripLeadingNullBytes(BigInteger.valueOf(i).toByteArray()));
+          contractAddress.add(contractCreator);
+          contractAddress.add(nonce);
+
+          String contract =
+              EthereumTools.hashKeccak(ByteUtilities.toHexString(contractAddress.encode()))
+                  .substring(96 / 4, 256 / 4);
+          FiatContractInterface contractClass = getContractType(contract);
+          if (contractClass != null) {
+            this.contractInterface = contractClass;
+            this.contractAddress = contract;
+            break;
+          }
+        }
+
+      } catch (Exception e) {
+        LOGGER.debug(null, e);
+      }
+    }
+
+    if ((contractAddress == null || contractAddress.isEmpty()) && config.generateNewContract()) {
+      LOGGER.debug("Generating new contract...");
+      // Create the TX data structure
+      RawTransaction tx = new RawTransaction();
+      tx.getGasPrice().setDecodedContents(ByteUtilities
+          .stripLeadingNullBytes(BigInteger.valueOf(config.getGasPrice()).toByteArray()));
+      tx.getGasLimit().setDecodedContents(ByteUtilities
+          .stripLeadingNullBytes(BigInteger.valueOf(config.getContractGas()).toByteArray()));
+      tx.getData().setDecodedContents(ByteUtilities.toByteArray(
+          contractInterface.getContractParameters()
+              .createContract(config.getAdminAccount(), Arrays.asList(config.getMultiSigAccounts()),
+                  config.getMinSignatures())));
+
+      String rawTx = ByteUtilities.toHexString(tx.encode());
+      LOGGER.debug("Creating contract: " + rawTx);
+      // TODO Sign and send it.
+    }
+  }
+
+  private FiatContractInterface getContractType(String contract) {
+    try {
+      String contractCode = ethereumRpc
+          .eth_getCode("0x" + contract.toLowerCase(Locale.US), DefaultBlock.LATEST.toString());
+      contractCode = contractCode.substring(2);
+      LOGGER.debug("Contract code: " + contractCode);
+      Class<?> contractType = FiatContract.class;
+      while (FiatContractInterface.class.isAssignableFrom(contractType)) {
+        FiatContractInterface contractParams = (FiatContractInterface) contractType.newInstance();
+        if (contractParams.getContractPayload().equalsIgnoreCase(contractCode)) {
+          return contractParams;
+        }
+        contractType = contractType.getSuperclass();
+      }
+    } catch (Exception e) {
+      LOGGER.debug(null, e);
+      return null;
+    }
+    return null;
+  }
 
   @Override
   public String createAddress(String name) {
@@ -40,19 +127,27 @@ public class FiatWallet implements Wallet {
   public String createAddress(String name, int skipNumber) {
     // Generate the next private key
     LOGGER.debug("Creating a new normal address...");
+    String user = EthereumTools.encodeUserKey(name);
+    if (!ownedAddresses.containsKey(user)) {
+      getAddresses(name);
+    }
+
     int rounds = 1 + skipNumber;
     String privateKey =
-        EthereumTools.getDeterministicPrivateKey(name, config.getServerKey(), rounds);
+        EthereumTools.getDeterministicPrivateKey(name, config.getServerPrivateKey(), rounds);
 
     // Convert to an Ethereum address
     String publicAddress = EthereumTools.getPublicAddress(privateKey);
 
     while (knownAddresses.contains(publicAddress.toLowerCase(Locale.US))) {
       rounds++;
-      privateKey = EthereumTools.getDeterministicPrivateKey(name, config.getServerKey(), rounds);
+      privateKey =
+          EthereumTools.getDeterministicPrivateKey(name, config.getServerPrivateKey(), rounds);
       publicAddress = EthereumTools.getPublicAddress(privateKey);
     }
     knownAddresses.add(publicAddress);
+
+    ownedAddresses.get(user).add(publicAddress);
 
     LOGGER.debug("New address " + publicAddress + " generated after " + rounds + " rounds");
     return publicAddress;
@@ -70,27 +165,66 @@ public class FiatWallet implements Wallet {
 
   @Override
   public Iterable<String> getAddresses(String name) {
-    // TODO Loop through addresses from name while they exist in the knownAddresses, I.E. those that have funds.
-    return null;
+    String user = EthereumTools.encodeUserKey(name);
+    if (!ownedAddresses.containsKey(user)) {
+      ownedAddresses.put(user, new HashSet<>());
+    }
+
+    HashSet<String> userAddresses = ownedAddresses.get(user);
+    if (userAddresses.isEmpty()) {
+      String balanceCheck = getBalance(createAddress(name));
+      while (balanceCheck != null && balanceCheck != BigDecimal.ZERO.toPlainString()) {
+        userAddresses = ownedAddresses.get(user);
+        balanceCheck = getBalance(createAddress(name));
+      }
+    }
+
+    return userAddresses;
   }
 
   @Override
   public String getMultiSigAddress(Iterable<String> addresses, String name) {
-    // TODO Due to the nature of this contract, this doesn't do anything. Just return the single-key address.
-    // The contract is multi-sig protected, but there's only one of them. So user-address is the differentiator.
-    return null;
+    return addresses.iterator().next();
   }
 
   @Override
   public String getBalance(String address) {
-    // TODO Lookup balance in the contract.
-    return null;
+    CallData callData = new CallData();
+    callData.setTo("0x" + contractAddress);
+    callData.setData("0x" + contractInterface.getContractParameters().getBalance(address));
+    callData.setGas("100000"); // Doesn't matter, just can't be nil
+    callData.setGasPrice("100000"); // Doesn't matter, just can't be nil
+    String response = ethereumRpc.eth_call(callData, DefaultBlock.LATEST.toString());
+
+    BigInteger balance = new BigInteger(1, ByteUtilities.toByteArray(response));
+    return balance.toString(10);
   }
 
   @Override
   public String createTransaction(Iterable<String> fromAddresses, Iterable<Recipient> toAddresses) {
-    // TODO Create a trading transaction for user-address balance
-    return null;
+    // Create the TX data structure
+    RawTransaction tx = new RawTransaction();
+    tx.getTo().setDecodedContents(ByteUtilities.toByteArray(contractAddress));
+    tx.getGasPrice().setDecodedContents(ByteUtilities
+        .stripLeadingNullBytes(BigInteger.valueOf(config.getGasPrice()).toByteArray()));
+    tx.getGasLimit().setDecodedContents(ByteUtilities
+        .stripLeadingNullBytes(BigInteger.valueOf(config.getContractGas()).toByteArray()));
+
+    List<String> recipients = new LinkedList<>();
+    List<Long> amounts = new LinkedList<>();
+    toAddresses.forEach(recipient -> {
+      amounts.add(recipient.getAmount().longValue());
+      recipients.add(recipient.getRecipientAddress());
+    });
+
+    // TODO Lookup nonce.
+
+    tx.getData().setDecodedContents(ByteUtilities.toByteArray(
+        contractInterface.getContractParameters()
+            .transfer(0L, fromAddresses.iterator().next(), recipients, amounts, new LinkedList<>(),
+                new LinkedList<>(), new LinkedList<>())));
+
+    return ByteUtilities.toHexString(tx.encode());
   }
 
   @Override
